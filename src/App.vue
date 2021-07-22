a90a66209395417e6c332bb47b981a650dee917f
Mike Zhou <mike.z@anzuk.education>
2021-07-22 11:53:16 +1000

    Add simple email validation

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <global-header :user="currentUser"></global-header>
   <div class="container">
+    <form action="">
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+        <input
+          type="text" class="form-control" id="exampleInputEmail1"
+          v-model="emailRef.val"
+          @blur="validateEmail"
+        >
+        <div class="form-text" v-if="emailRef.error">{{emailRef.message}}</div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密码</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+    </form>
     <column-list :list="list"></column-list>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
-import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
-const currentUser: UserProps = {
+import GlobalHeader, { UserPorps } from './components/GlobalHeader.vue'
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+const currentUser: UserPorps = {
   isLogin: true,
   name: 'Michael'
 }
@@ -19,33 +35,49 @@ const testData: ColumnProps[] = [
     id: 1,
     title: 'test1的专栏',
     description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+    avatar: 'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3092529483,2095048025&fm=26&gp=0.jpg'
   },
   {
     id: 2,
     title: 'test2的专栏',
     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+    avatar: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2400920312,4079163394&fm=26&gp=0.jpg'
   },
   {
     id: 3,
     title: 'test1的专栏',
     description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+    avatar: 'https://dss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3107801461,4048012422&fm=26&gp=0.jpg'
   },
   {
     id: 4,
-    title: 'test4的专栏',
+    title: 'test2的专栏',
     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+    avatar: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3796312028,1081481082&fm=26&gp=0.jpg'
   }
 ]
 export default defineComponent({
   name: 'App',
   setup (props) {
+    const emailRef = reactive({
+      val: '',
+      error: false,
+      message: ''
+    })
+    const validateEmail = () => {
+      if (emailRef.val.trim() === '') {
+        emailRef.error = true
+        emailRef.message = 'can not be empty'
+      } else if (!emailReg.test(emailRef.val)) {
+        emailRef.error = true
+        emailRef.message = 'should be valid email'
+      }
+    }
     return {
       list: testData,
-      currentUser: currentUser
+      currentUser: currentUser,
+      emailRef,
+      validateEmail
     }
   },
   components: {
@@ -54,3 +86,6 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss">
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <form action="">
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+        <input
+          type="text" class="form-control" id="exampleInputEmail1"
+          v-model="emailRef.val"
+          @blur="validateEmail"
+        >
+        <div class="form-text" v-if="emailRef.error">{{emailRef.message}}</div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密码</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+export default defineComponent({
+  name: 'form',
+  setup (props) {
+    const emailRef = reactive({
+      val: '',
+      error: false,
+      message: ''
+    })
+    const validateEmail = () => {
+      if (emailRef.val.trim() === '') {
+        emailRef.error = true
+        emailRef.message = 'can not be empty'
+      } else if (!emailRef.test(emailRef.val)) {
+        emailRef.error = true
+        emailRef.message = 'should be valid email'
+      }
+    }
+    return {
+      emailRef,
+      validateEmail
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
